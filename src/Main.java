c7acbf25046fdb5538b415a98786119d43b40c8d
Sergey <tel.cromax@gmail.com>
2021-07-12 22:10:32 +1100

    correct REPORT.md

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // TODO: подставлять номер карты нужно сюда между двойными кавычками, без пробелов
-        String number = "5102373507172374";
+        String number = "4664015016514532611";
         System.out.println(String.format("Result is %s", isValidCardNumber(number) ? "OK" : "FAIL"));
     }
 
